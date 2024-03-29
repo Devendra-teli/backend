ec26806c3bd0a565ddfca4bef995d12c9f756aaa
Devendra-teli <devendrateliofficial@gmail.com>
2024-03-29 14:16:20 +0530

    connect mongo atlas

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// mongodb + srv://devendrateliMD:devendrateliofficialMD@cluster0.cs66dfj.mongodb.net/
+
+// mongodb+srv://devendrateliMD:<password>@cluster0.cs66dfj.mongodb.net/
+
+export const DB_NAME = "videotube"
